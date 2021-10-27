12dcb2db4c5e333e3b58d3bc84f16e006ce9a779
mrkendrick <charlesrichson@gmail.com>
2021-10-27 01:09:13 +0100

    Partner page responsiveness

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,102 +1,123 @@
-import React from 'react'
-import logo from '../../../src/assets/Images/logo.png'
+import React from 'react';
+import logo from '../../../src/assets/Images/logo.png';
 import { Link } from 'react-router-dom';
 import './footer.scss';
-import Twitter from "./../../assets/Svg/Twitter"
-import Telegram from "./../../assets/Svg/Telegram"
-import Instagram from "./../../assets/Svg/Instagram"
-import Linkedin from "./../../assets/Svg/Linkedin"
-import Reddit from "./../../assets/Svg/Reddit"
-import Facebook from "./../../assets/Svg/Facebook"
+import Twitter from './../../assets/Svg/Twitter';
+import Telegram from './../../assets/Svg/Telegram';
+import Instagram from './../../assets/Svg/Instagram';
+import Linkedin from './../../assets/Svg/Linkedin';
+import Reddit from './../../assets/Svg/Reddit';
+import Facebook from './../../assets/Svg/Facebook';
 export default function Footer() {
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log('You just Sign up for our newsletter');
-      }
-    return (
-        <div className='footer'>
-            <footer>
-                <div className='footer-container'>
-                    <div className="footer-img">
-                        <img src={logo} alt=" "/>
-                    </div>
-                    <div className="footer-title">
-                        Educating the world <br />
-                        about its cultures through NFTs
-                    </div>
-                    <div className="footer-form">
-                        <form action="newsletter" method="post" onSubmit={handleSubmit}>
-                            <label htmlFor="newsletter" className='newsletter-label'>
-                                Sign up for our newsletter <br />
-                                <div className="value-btn-box">
-                                    <input type="email" name="newsletter" placeholder='Your Email' id="newsletter" />
-                                    {/* <input type="button" value="Sign up" className="newsletter-btn"/> */}
-                                    <button className="newsletter-btn" type="submit">Sign Up</button>
-                                </div>
-                            </label>
-                        </form>
-                    </div>
-                    <div className="footer-list">
-                        <div className="footer-menu">
-                            Menu
-                            <ul>
-                                <li><Link to='/contact'>Contact us</Link></li>
-                                <li><Link to='/partners'>Our Partners</Link></li>
-                                <li><Link to='/team'>Our Team</Link></li>
-                                <li><Link to='/whitepaper'>Whitepaper</Link></li>
-                                <li><Link to='/blog'>Blog</Link></li>
-                            </ul>
-                        </div>
-                        <div className="footer-service">
-                            Service
-                            <ul>
-                                <li><Link to='#'>Single NFTs</Link></li>
-                                <li><Link to='#'>Collection NFTs</Link></li>
-                                <li><Link to='#'>GameFI</Link></li>
-                                <li><Link to='#'>Real Estate</Link></li>
-                                <li><Link to='#'>Harmonic Studio</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="footer-icons">
-                        <ul>
-                            <li>
-                                <Twitter />
-                            </li>
-                            <li>
-                                <Telegram />
-                            </li>
-                            <li> 
-                                <Instagram />
-                            </li>
-                            <li>
-                                <Linkedin />
-                            </li>
-                            <li>
-                                <Reddit />
-                           </li>   
-                            <li>
-                                <Facebook />
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="footer-copyright">
-                        <div className="copy-text">
-                            © ArtMemoir. All rights reserved
-                        </div>
-                        <div className="terms">
-                            <Link to='/terms'>
-                                Terms and Conditions
-                            </Link>
-                        </div>
-                        <div className="privacy">
-                            <Link to='privacy'>
-                                Privacy
-                            </Link>
-                        </div>
-                    </div>
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('You just Sign up for our newsletter');
+  }
+  return (
+    <div className="footer">
+      <footer>
+        <div className="footer-container">
+          <div className="footer-img">
+            <img src={logo} alt=" " />
+          </div>
+          <div className="footer-title">
+            Educating the world <br />
+            about its cultures through NFTs
+          </div>
+          <div className="footer-form">
+            <form action="newsletter" method="post" onSubmit={handleSubmit}>
+              <label htmlFor="newsletter" className="newsletter-label">
+                Sign up for our newsletter <br />
+                <div className="value-btn-box">
+                  <input
+                    type="email"
+                    name="newsletter"
+                    placeholder="Your Email"
+                    id="newsletter"
+                  />
+                  {/* <input type="button" value="Sign up" className="newsletter-btn"/> */}
+                  <button className="newsletter-btn" type="submit">
+                    Sign Up
+                  </button>
                 </div>
-            </footer>
+              </label>
+            </form>
+          </div>
+          <div className="footer-list">
+            <div className="footer-menu">
+              Menu
+              <ul>
+                <li>
+                  <Link to="/contact">Contact us</Link>
+                </li>
+                <li>
+                  <Link to="/partners">Our Partners</Link>
+                </li>
+                <li>
+                  <Link to="/team">Our Team</Link>
+                </li>
+                <li>
+                  <Link to="/whitepaper">Whitepaper</Link>
+                </li>
+                <li>
+                  <Link to="/blog">Blog</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-service">
+              Service
+              <ul>
+                <li>
+                  <Link to="#">Single NFTs</Link>
+                </li>
+                <li>
+                  <Link to="#">Collection NFTs</Link>
+                </li>
+                <li>
+                  <Link to="#">GameFI</Link>
+                </li>
+                <li>
+                  <Link to="#">Real Estate</Link>
+                </li>
+                <li>
+                  <Link to="#">Harmonic Studio</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="footer-icons">
+            <ul>
+              <li>
+                <Twitter />
+              </li>
+              <li>
+                <Telegram />
+              </li>
+              <li>
+                <Instagram />
+              </li>
+              <li>
+                <Linkedin />
+              </li>
+              <li>
+                <Reddit />
+              </li>
+              <li>
+                <Facebook />
+              </li>
+            </ul>
+          </div>
+          <div className="footer-copyright">
+            <div className="copy-text">© ArtMemoir. All rights reserved</div>
+            <div className="terms">
+              <Link to="/terms">Terms and Conditions</Link>
+            </div>
+            <div className="privacy">
+              <Link to="/privacy">Privacy</Link>
+            </div>
+          </div>
         </div>
-    )
+      </footer>
+    </div>
+  );
 }
