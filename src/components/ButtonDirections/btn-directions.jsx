ee371ee6c72d9b503a293c603393d70142285e29
mrkendrick <charlesrichson@gmail.com>
2021-10-29 18:40:15 +0100

    White paper mobile implementation

--- src/components/ButtonDirections/btn-directions.jsx
+++ src/components/ButtonDirections/btn-directions.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
-import './btn-directions.scss'
+import './btn-directions.scss';
 export default function ButtonDirections() {
-    return (
-        <div className="btn-direction">
-            <div className="arrow-left"> 
-                <FaAngleLeft />
-            </div>
-            <div className="arrow-right">
-                <FaAngleRight />
-            </div>
-        </div>
-    )
+  return (
+    <div className="btn-direction">
+      <div className="arrow-left">
+        <FaAngleLeft />
+      </div>
+      <div className="arrow-right">
+        <FaAngleRight />
+      </div>
+    </div>
+  );
 }
--- src/components/DropDowns/DropDown.jsx
+++ src/components/DropDowns/DropDown.jsx
@@ -1,17 +1,27 @@
-import React, { useState }  from 'react'
+import React, { useState } from 'react';
 import 'react-dropdown/style.css';
-import { FaAngleUp, FaAngleDown} from 'react-icons/fa';
-import './dropdown.scss'
+import { FaAngleUp, FaAngleDown } from 'react-icons/fa';
+import './dropdown.scss';
 
 function DropDowns({ selected, setSelected }) {
   const [isActive, setIsActive] = useState(false);
-  const options = ["Overview", "NFTs & Art", "NFTs & Real Estate", "NFTs & Games", "Art Memoir Services", "Art Memoir NFT Utilities", "Art Memoir Economics", "The team", "The Disclaimer"];
+  const options = [
+    'Overview',
+    'NFTs & Art',
+    'NFTs & Real Estate',
+    'NFTs & Games',
+    'Art Memoir Services',
+    'Art Memoir NFT Utilities',
+    'Art Memoir Economics',
+    'The team',
+    'The Disclaimer',
+  ];
   return (
     <div className="dropdown">
       <div className="dropdown-btn" onClick={(e) => setIsActive(!isActive)}>
         {selected}
-        <span className='dropdowns-icons'> 
-            {isActive ? <FaAngleUp /> : <FaAngleDown />}
+        <span className="dropdowns-icons">
+          {isActive ? <FaAngleUp /> : <FaAngleDown />}
         </span>
       </div>
       {isActive && (
