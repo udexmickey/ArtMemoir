baafe2cfca3e71902631f4c9edd2ee93891cf449
mrkendrick <charlesrichson@gmail.com>
2021-10-31 13:47:40 +0100

    Blog mobile responsiveness

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,65 +1,62 @@
 import React from 'react';
 import UserCard from '../../components/UserCard';
-import Background from '../../assets/Images/blogBacground.png'
+import Background from '../../assets/Images/blogBacground.png';
 
-import "./blogs.scss"
+import './blogs.scss';
 import Blogs from './blogsData';
 import ButtonDirections from '../../components/ButtonDirections/btn-directions';
 
 export default function Blog() {
-    const blogposts = Blogs.map( 
-        function BlogCard(blog) {
-           return ( 
-                <div className="blog-card">
-                    <UserCard 
-                        key={blog.player.id}
-                        avatar={blog.player.avatar} 
-                        name={blog.player.name}
-                        role={blog.player.role}
-                        className={``}
-                        message={blog.player.message}
-                        btn={blog.player.btn}
-                        link={blog.player.link}
-                    />
-                </div>
-
-            )
-            
-        }
-    )
-
+  const blogposts = Blogs.map(function BlogCard(blog) {
     return (
-        <div className='blog-page'>
-            {/* <img src={Background} alt="" srcset="" style={{width: '100%'}}  /> */}
-            
-            <div className="blog-badge" style={{background: `url(${Background})`, backgroundSize: '100%', width: '100%', backgroundRepeat: 'no-repeat'}}>
-                <div className="blog-heading-container">
-                    <div className="blog-heading-holder">
-                        <div className="blog-heading-title">
-                            Tempus at cursus maecenas erat id aenean pharetra.
-                        </div>
-                        <div className="blog-heading-text">
-                        Amet auctor ac sed vel sed. Augue vel nec, ut gravida quis et. Pretium eu amet tempus elit, interdum consectetur arcu tortor. Pharetra pulvinar accumsan non facilisis amet. Lacus in.
-                        </div>
-                    </div>
-                </div>
-            </div>
+      <div className="blog-card">
+        <UserCard
+          key={blog.player.id}
+          avatar={blog.player.avatar}
+          name={blog.player.name}
+          role={blog.player.role}
+          className={``}
+          message={blog.player.message}
+          btn={blog.player.btn}
+          link={blog.player.link}
+        />
+      </div>
+    );
+  });
 
-            <div className="blog-container">
-                <div className="blog-card-holder">
-                    {blogposts }
-                </div>
+  return (
+    <div className="blog-page">
+      {/* <img src={Background} alt="" srcset="" style={{width: '100%'}}  /> */}
+
+      <div
+        className="blog-badge"
+        style={{
+          background: `url(${Background})`,
+          backgroundSize: '100%',
+          width: '100%',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <div className="blog-heading-container">
+          <div className="blog-heading-holder">
+            <div className="blog-heading-title">
+              Tempus at cursus maecenas erat id aenean pharetra.
             </div>
-            <ButtonDirections />
+            <div className="blog-heading-text">
+              Amet auctor ac sed vel sed. Augue vel nec, ut gravida quis et.
+              Pretium eu amet tempus elit, interdum consectetur arcu tortor.
+              Pharetra pulvinar accumsan non facilisis amet. Lacus in.
+            </div>
+          </div>
         </div>
-    )
+      </div>
+
+      <div className="blog-container">
+        <div className="blog-card-holder">{blogposts}</div>
+      </div>
+      <ButtonDirections />
+    </div>
+  );
 }
 
-
-
-<li className='listen'>second</li>
-                
-                
-                
-                
-                
+<li className="listen">second</li>;
