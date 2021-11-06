0454ef893786105113b90729d7e514e0200b9c61
udexmickey <jerrymike80@yahoo.com>
2021-11-06 10:08:40 +0100

    updated

--- src/components/Showcase/index.jsx
+++ src/components/Showcase/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import pic1 from '../../assets/Images/pic1.png';
-import pic2 from '../../assets/Images/pic2.png';
-import pic3 from '../../assets/Images/pic3.png';
-// import pic4 from '../../assets/Images/pic4.png';
+import pic1 from '../../assets/Images/nft--africa.JPG';
+import pic2 from '../../assets/Images/nft--town.JPG';
+import pic3 from '../../assets/Images/nft-bird.JPG';
+import pic4 from '../../assets/Images/nft-boy.JPG';
+import pic5 from '../../assets/Images/nft-bubbles.JPG';
+import pic6 from '../../assets/Images/nft-eye.JPG';
+import pic7 from '../../assets/Images/nft-girl.JPG';
+import pic8 from '../../assets/Images/nft-head.JPG';
+import pic9 from '../../assets/Images/nft-lady.JPG';
+import pic10 from '../../assets/Images/nft-lion.JPG';
 import './showcase.scss';
 
 const Showcase = () => {
@@ -19,10 +25,25 @@ const Showcase = () => {
           <img className="showcase-img" src={pic3} alt="" />
         </div>
         <div>
-          <img className="showcase-img" src={pic3} alt="" />
+          <img className="showcase-img" src={pic4} alt="" />
         </div>
         <div>
-          <img className="showcase-img" src={pic3} alt="" />
+          <img className="showcase-img" src={pic5} alt="" />
+        </div>
+        <div>
+          <img className="showcase-img" src={pic6} alt="" />
+        </div>
+        <div>
+          <img className="showcase-img" src={pic7} alt="" />
+        </div>
+        <div>
+          <img className="showcase-img" src={pic8} alt="" />
+        </div>
+        <div>
+          <img className="showcase-img" src={pic9} alt="" />
+        </div>
+        <div>
+          <img className="showcase-img" src={pic10} alt="" />
         </div>
         {/* <div>
           <img className="showcase-img" src={pic4} alt="" />
