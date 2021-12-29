7e963654035bfa4723bbb043c4b8e781fd312746
udexmickey <jerrymike80@yahoo.com>
2021-12-29 12:12:38 +0100

    update

--- src/components/Showcase6/Item.jsx
+++ src/components/Showcase6/Item.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../assets/Images/logo-xs.png';
+// import logo from '../../assets/Images/logo-xs.png';
 
 const Item = ({ id, img, header, content, link }) => {
   return (
