35c2dfabb79aa10cc05ba829b1474d8580d0d517
udexmickey <jerrymike80@yahoo.com>
2021-12-30 16:35:55 +0100

    update

---
+++ src/components/RoadMap/Roadmapdesktop.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './roadmapdesktop';
+
+export default function Roadmapdesktop() {
+    return (
+        <div className="roadmapdesktop">
+            <main>
+                {/* <!-- learning german is hard --> */}
+                <p><span class="wave">Vergangenheitsbewältigungskultur</span></p>
+            </main>
+        </div>
+    )
+}
