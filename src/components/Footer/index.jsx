27ce603cccdfbf6d8f33e37fed756f16715bb5e9
Charles Duruaku <charlesrichson@gmail.com>
2021-12-31 17:24:13 +0100

    linked footer elements

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,39 +1,39 @@
-import React, { useState } from 'react';
-import logo from '../../../src/assets/Images/logo.png';
-import { Link } from 'react-router-dom';
-import './footer.scss';
-import { ImLink } from 'react-icons/all';
-import IconList from '../IconList/IconList';
-import {url} from '../../config/config.json'
-import { useForm } from 'react-hook-form';
-import axios from 'axios';
+import React, { useState } from 'react'
+import logo from '../../../src/assets/Images/logo.png'
+import { Link } from 'react-router-dom'
+import './footer.scss'
+import { ImLink } from 'react-icons/all'
+import IconList from '../IconList/IconList'
+import { url } from '../../config/config.json'
+import { useForm } from 'react-hook-form'
+import axios from 'axios'
 // import Test from '../../pages/Test';
 // import { functions } from 'lodash';
 
 export default function Footer() {
-
   const [signletter, setSignLetter] = useState('Signup')
-  const { register, handleSubmit, formState, setValue, error } = useForm();
-  const { isSubmitting } = formState;
+  const { register, handleSubmit, formState, setValue, error } = useForm()
+  const { isSubmitting } = formState
   const handleneswletterSubmit = async data => {
     // e.preventDefault();
-    setValue('email');
+    setValue('email')
 
     axios
-    .post(`${url}subscribe`, data)
-    .then(res => (res))
-    .catch(err => (err));
-    if (error) console.log('email must be unique');
+      .post(`${url}subscribe`, data)
+      .then(res => res)
+      .catch(err => err)
+    if (error) console.log('email must be unique')
 
-      // return promise that resolves after 2 seconds
-      return new Promise(resolve => {
-        setTimeout(() => {
-            resolve();
-            <div className='successfulsent' style={{backgroundColor: 'green'}}>{setSignLetter('Sent')}</div>;
-        }, 2000) ;
-        <div style={{background: 'green'}}>{setSignLetter('Loading')}</div>
-    });
-       
+    // return promise that resolves after 2 seconds
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve()
+        ;<div className='successfulsent' style={{ backgroundColor: 'green' }}>
+          {setSignLetter('Sent')}
+        </div>
+      }, 2000)
+      ;<div style={{ background: 'green' }}>{setSignLetter('Loading')}</div>
+    })
   }
 
   // const sendLetter = () => {
@@ -44,27 +44,35 @@ export default function Footer() {
   //   return signletter;
   // }
   return (
-    <div className="footer">
+    <div className='footer'>
       <footer>
-        <div className="footer-container">
-          <div className="footer-img">
-            <img loading='lazy' src={logo} alt=" " />
+        <div className='footer-container'>
+          <div className='footer-img'>
+            <img loading='lazy' src={logo} alt=' ' />
           </div>
-          <div className="footer-title">
-          Exploring the world of Art through NFTs.
+          <div className='footer-title'>
+            Exploring the world of Art through NFTs.
           </div>
-          <div className="footer-form">
-            <form action={`${url}subscribe`} method="post" onSubmit={handleSubmit(handleneswletterSubmit)}>
+          <div className='footer-form'>
+            <form
+              action={`${url}subscribe`}
+              method='post'
+              onSubmit={handleSubmit(handleneswletterSubmit)}
+            >
               {/* <Test name={`Sign Up`}/> */}
-              <label htmlFor="newsletter" className="newsletter-label">
-                <span>Sign up for our newsletter </span><br />
-                <small>Stay up to date on new releases, interviews, events, and updates from ArtMemoir’s community.</small>
-                <div className="value-btn-box">
+              <label htmlFor='newsletter' className='newsletter-label'>
+                <span>Sign up for our newsletter </span>
+                <br />
+                <small>
+                  Stay up to date on new releases, interviews, events, and
+                  updates from ArtMemoir’s community.
+                </small>
+                <div className='value-btn-box'>
                   <input
-                    type="email"
-                    name="newsletter"
-                    placeholder="Your Email"
-                    id="newsletter"
+                    type='email'
+                    name='newsletter'
+                    placeholder='Your Email'
+                    id='newsletter'
                     {...register('email', { required: true })}
                   />
                   {/* <input type="button" value="Sign up" className="newsletter-btn"/> */}
@@ -72,59 +80,68 @@ export default function Footer() {
                     Sign Up
                   </button> */}
                   {/* <Test name={`Sign Up`}/> */}
-                  <button disabled={isSubmitting} style={{cursor : 'pointer'}} className="newsletter-btn" type='submit' >
-                  {isSubmitting && <span style={{marginRight: '.5rem'}}><i class="fa fa-refresh fa-spin"></i></span>}
-                       {signletter}
+                  <button
+                    disabled={isSubmitting}
+                    style={{ cursor: 'pointer' }}
+                    className='newsletter-btn'
+                    type='submit'
+                  >
+                    {isSubmitting && (
+                      <span style={{ marginRight: '.5rem' }}>
+                        <i class='fa fa-refresh fa-spin'></i>
+                      </span>
+                    )}
+                    {signletter}
                   </button>
                 </div>
               </label>
             </form>
           </div>
-          <div className="footer-list">
-            <div className="footer-menu">
-              <span className="footer-list-title">Menu</span>
+          <div className='footer-list'>
+            <div className='footer-menu'>
+              <span className='footer-list-title'>Menu</span>
               <ul>
                 <li>
-                  <Link to="/contact">Contact us</Link>
+                  <Link to='/contact'>Contact us</Link>
                 </li>
                 <li>
-                  <Link to="/partners">Our Partners</Link>
+                  <Link to='/partners'>Our Partners</Link>
                 </li>
                 <li>
-                  <Link to="/team">Our Team</Link>
+                  <Link to='/team'>Our Team</Link>
                 </li>
                 <li>
-                  <Link to="/whitepaper">Whitepaper</Link>
+                  <Link to='/whitepaper'>Whitepaper</Link>
                 </li>
                 <li>
-                  <Link to="/blog">Blog</Link>
+                  <Link to='/blog'>Blog</Link>
                 </li>
               </ul>
             </div>
-            <div className="footer-service">
-              <span className="footer-list-title">Service</span>
+            <div className='footer-service'>
+              <span className='footer-list-title'>Service</span>
               <ul>
                 <li>
-                  <Link to="#">Single NFTs</Link>
+                  <a href='#art'>Single NFTs</a>
                 </li>
                 <li>
-                  <Link to="#">Collection NFTs</Link>
+                  <a href='#art'>Collection NFTs</a>
                 </li>
                 <li>
-                  <Link to="#">GameFI</Link>
+                  <a href='#game-fi'>GameFI</a>
                 </li>
                 <li>
-                  <Link to="#">Real Estate</Link>
+                  <a href='#real-estate'>Real Estate</a>
                 </li>
                 <li>
-                  <Link to="#">RoadMap</Link>
+                  <a href='#roadmap'>RoadMap</a>
                 </li>
               </ul>
             </div>
           </div>
           <IconList />
-          <div className="footer-copyright">
-            <div className="copy-text">© ArtMemoir. All rights reserved</div>
+          <div className='footer-copyright'>
+            <div className='copy-text'>© ArtMemoir. All rights reserved</div>
             {/* <div className="terms-privacy">
               <div className="terms">
               <ImLink /> <Link to="https://www.harmonicstudioz.com/"> Powered by HarmonicStudioz </Link>
@@ -134,11 +151,14 @@ export default function Footer() {
               </div>
             </div>  */}
           </div>
-          <div className="footer-dev">
-          <ImLink />  <Link to="https://www.harmonicstudioz.com/">Powered by HarmonicStudioz </Link>
-            </div>
+          <div className='footer-dev'>
+            <ImLink />{' '}
+            <Link to='https://www.harmonicstudioz.com/'>
+              Powered by HarmonicStudioz{' '}
+            </Link>
+          </div>
         </div>
       </footer>
     </div>
-  );
+  )
 }
--- src/components/RoadMap/index.jsx
+++ src/components/RoadMap/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import './roadmap.scss'
 
 const RoadMap = props => {
-    return (
-        <div className='roadmap'>
-            {/* <table>
+  return (
+    <div className='roadmap'>
+      {/* <table>
             <tr>
                 <th>Company</th>
                 <th>Contact</th>
@@ -21,81 +21,100 @@ const RoadMap = props => {
                 <td>Mexico</td>
             </tr>
             </table> */}
-            <div className="roadmap-holder">
-                <div className="round-table-top"> 2021</div>
-                <div className="roadmap-stick"> 
-                    <div className="roadmap-stick-holder">
-                        <div className="span-border-line">
-                            <div className="dot-map">
-                            <div className="map-content">
-                                <div className="map-title"> Q4 </div>
-                                <div className="map-message"> - Project Development</div>
-                                <div className="map-message"> - Single Nfts lunch</div>
-                                <div className="map-message"> - Website Development</div>
-                                <div className="map-message"> - Cross Continent Collaboration (CNFt) Partnership</div>
-                                <div className="map-message"> - Introduction of The Seven Continents</div>
-                                <div className="map-message"> - On boarding Artist for The African Continent</div>
-
-                            </div>
-                            </div>
-                        </div>
-                        <div className="map-title-next"> 2022 </div>
-
-                        <div className="span-border-line">
-                            <div className="dot-map">
-                            <div className="map-content">
-                                <div className="map-title"> Q1 </div>
-                                <div className="map-message"> - Phrase I Marketing</div>
-                                <div className="map-message"> - Token utility Partnership ( African Continent)</div>
-                                <div className="map-message"> - Lunch of The African Continen</div>
-
-                            </div>
-                            </div>
-                        </div>
-                        <div className="span-border-line">
-                            <div className="dot-map">
-                            <div className="map-content">
-                                <div className="map-title"> Q2 </div>
-                                <div className="map-message"> - Partnerships</div>
-                                <div className="map-message"> - Stake and Earn Partnership</div>
-                                <div className="map-message"> - Phrase II Marketing</div>
-                                <div className="map-message"> - New Nft utility unveil</div>
-                                <div className="map-message"> - Cross Community Games</div>
-
-                            </div>
-                            </div>
-                        </div>
-                        <div className="span-border-line">
-                            <div className="dot-map">
-                            <div className="map-content">
-                                <div className="map-title"> Q3 </div>
-                                <div className="map-message"> - NFT utilities (Unveil)</div>
-                                <div className="map-message"> - Introducing Africa’s First Game (Based on African Mythology) Marketing Phase 3</div>
-                                <div className="map-message"> - Gamefi Partnership</div>
-                                <div className="map-message"> - Introduction of The Asia Continent</div>
-
-                            </div>
-                            </div>
-                        </div>
-                    </div>
+      <div className='roadmap-holder'>
+        <div className='round-table-top'> 2021</div>
+        <div className='roadmap-stick'>
+          <div className='roadmap-stick-holder'>
+            <div className='span-border-line'>
+              <div className='dot-map'>
+                <div className='map-content'>
+                  <div className='map-title'> Q4 </div>
+                  <div className='map-message'> - Project Development</div>
+                  <div className='map-message'> - Single Nfts lunch</div>
+                  <div className='map-message'> - Website Development</div>
+                  <div className='map-message'>
+                    {' '}
+                    - Cross Continent Collaboration (CNFt) Partnership
+                  </div>
+                  <div className='map-message'>
+                    {' '}
+                    - Introduction of The Seven Continents
+                  </div>
+                  <div className='map-message'>
+                    {' '}
+                    - On boarding Artist for The African Continent
+                  </div>
                 </div>
-                <div className="round-table-bottom">2022</div>
-                <div id="background">
-                    <ul class="container">
-                        <li class="backward">R</li>
-                        <li class="backward">O</li>
-                        <li class="backward">A</li>
-                        <li class="backward">D</li>
-                        <li class="backward">M</li>
-                        <li class="backward">A</li>
-                        <li class="backward">P</li>
-                    </ul>
-                </div>
+              </div>
             </div>
+            <div className='map-title-next'> 2022 </div>
 
-
+            <div className='span-border-line'>
+              <div className='dot-map'>
+                <div className='map-content'>
+                  <div className='map-title'> Q1 </div>
+                  <div className='map-message'> - Phrase I Marketing</div>
+                  <div className='map-message'>
+                    {' '}
+                    - Token utility Partnership ( African Continent)
+                  </div>
+                  <div className='map-message'>
+                    {' '}
+                    - Lunch of The African Continen
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='span-border-line'>
+              <div className='dot-map'>
+                <div className='map-content'>
+                  <div className='map-title'> Q2 </div>
+                  <div className='map-message'> - Partnerships</div>
+                  <div className='map-message'>
+                    {' '}
+                    - Stake and Earn Partnership
+                  </div>
+                  <div className='map-message'> - Phrase II Marketing</div>
+                  <div className='map-message'> - New Nft utility unveil</div>
+                  <div className='map-message'> - Cross Community Games</div>
+                </div>
+              </div>
+            </div>
+            <div className='span-border-line'>
+              <div className='dot-map'>
+                <div className='map-content'>
+                  <div className='map-title'> Q3 </div>
+                  <div className='map-message'> - NFT utilities (Unveil)</div>
+                  <div className='map-message'>
+                    {' '}
+                    - Introducing Africa’s First Game (Based on African
+                    Mythology) Marketing Phase 3
+                  </div>
+                  <div className='map-message'> - Gamefi Partnership</div>
+                  <div className='map-message'>
+                    {' '}
+                    - Introduction of The Asia Continent
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+        <div className='round-table-bottom'>2022</div>
+        <div id='background'>
+          <ul class='container'>
+            <li class='backward'>R</li>
+            <li class='backward'>O</li>
+            <li class='backward'>A</li>
+            <li class='backward'>D</li>
+            <li class='backward'>M</li>
+            <li class='backward'>A</li>
+            <li class='backward'>P</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default RoadMap
